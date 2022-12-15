7dbef48514d864dcee1618f3d7330474e2688d65
crescentarc <matthew.seth.taganile@gmail.com>
2022-12-15 16:51:23 -0500

    Add variables and a div to display scores

--- script.js
+++ script.js
@@ -12,31 +12,44 @@ function game() {
     resultsDisplay.style.marginTop = '30px'
     container.appendChild(resultsDisplay);
 
+    // constants for both sides' scores
+    let playerScore = 0;
+    let cpuScore = 0;
+    const scoreDisplay = document.createElement('div');
+    scoreDisplay.style.marginTop = '30px';
+    container.appendChild(scoreDisplay);
+
     //create event listener for 3 buttons
     const buttons = document.querySelectorAll('button');
     buttons.forEach((button) =>  {
         button.addEventListener('click', () => {
             playerSelection = button.innerText;
             computerSelection = cpuPlay(); 
-            playRound(playerSelection, computerSelection);
             resultsDisplay.textContent = playRound(playerSelection, computerSelection);
+            scoreDisplay.textContent = 'Player Score: ' + playerScore + ' / CPU Score: ' + cpuScore;
         })
 
         //function to judge round outcome
         function playRound (playerSelection, computerSelection) {
             if (playerSelection == computerSelection) {
-                return 'Tie!';
+                return 'Tie! Try again!';
             } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+                playerScore++;
                 return 'You win! Rock beats Scissors!';
             } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+                playerScore++;
                 return 'You win! Paper Beats Rock!';
             } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+                playerScore++;
                 return 'You win! Scissors Beats Paper!';
             } else if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
+                cpuScore++;
                 return 'You Lose. Rock Beats Scissors.';
             } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
+                cpuScore++;
                 return 'You Lose. Paper Beats Rock.';
             } else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
+                cpuScore++;
                 return 'You Lose. Scissors Beats Paper'
             };
         }
